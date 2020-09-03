a20f5d479dfc3228bdc5d6286d42dd626bbd6aee
David Caldwell <dwclogic@gmail.com>
2020-09-03 15:14:39 -0600

    added Echo Service to Tyk

--- samples/docker.sh
+++ samples/docker.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
+
 # toyed around with a network
 docker network create tyk-community-network
-44f9f88692721aef3c55f056001ed24a75a182dd4c49fe588dfcf0c5292c8a1b
 
 # Tutorial https://hub.docker.com/r/tykio/tyk-gateway
-
 # start the redis up no config yet
 docker pull redis
 docker run -d --name tyk_redis redis
-38e2f3f05e4021a9adfdb0790952ae4b3745804b0647627d7ee2508717cb9c15
-
 
 # Was using this but no setup was included so I found the other mongo with express stack
 # docker pull mongo
@@ -26,3 +23,5 @@ docker run -d --name tyk_gateway -p 8080:8080 \
   -v $(pwd)/apps:/opt/tyk-gateway/apps \
   -v $(pwd)/policies:/opt/tyk-gateway/policies \
   tykio/tyk-gateway
+
+# started
